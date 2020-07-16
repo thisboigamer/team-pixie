982d9f8d3c122b92bf0fadcd266a6db9ef3a155b
Luke Heffernan <lukeheffobro@gmail.com>
2020-07-16 11:18:21 -0500

    Pickups Scripts
    All of the scripts to pickup the items and unlock the end of the game.

---
+++ team-pixie/Assets/door.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            if (GameVariable.itemCount > 2)
+            {
+                print("enter");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ team-pixie/Assets/pickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player");
+        {
+            print ("Item picked up");
+            GameVariable.itemCount += 1;
+            Destroy(gameObject);
+        }
+    }
+}
