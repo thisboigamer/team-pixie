ca0bdfb4a50eb481ff361a13c56ba669ee8f91a5
Caveman <tlha2422@gmail.com>
2020-07-16 12:13:46 -0500

    Added random item spawn script and added a manager object

---
+++ team-pixie/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    //UI Slider with Max value of 100 and min value of 0
+    [SerializeField] Slider HealthSlider;
+
+    //Base Starting Health
+    [SerializeField] float Health = 100f;
+
+    //ManagerObject
+    private GameObject Manager;
+    void Start()
+    {
+        HealthSlider.value = Health;
+        HealthSet();
+        //Game Manager needs Manager Tag
+        Manager = GameObject.FindGameObjectWithTag("Manager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Callse DecayHealth Every Second
+        Invoke("DecayHealth", 1);
+
+        //Checks to see when Health is Depleted
+        if (Health <= 1)
+        {
+            //Manager.GetComponent<ManagerScript>().GameOver();
+        }
+    }
+
+
+    //Decaying health Function
+    void DecayHealth()
+    {
+        Health -= 0.1f;
+        UpdateUI();
+    }
+
+    //Clamps Health Between 0 and 100
+    void HealthSet()
+    {
+        Health = Mathf.Clamp(Health, 0f, 100f);
+    }
+
+    //Updates UI
+    void UpdateUI()
+    {
+        HealthSlider.value = Health;
+    }
+
+    //ResetsHealth 
+    //Should be called whenever player Picks up PixieStix
+    public void HealthUp()
+    {
+        Health = 100;
+    }
+}
